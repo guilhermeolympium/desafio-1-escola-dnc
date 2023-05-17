a81f0873f321eeb1c9e87c58e551a10ce9aa246d
Guilherme Dias Olímpio <playerimmortal@outlook.com>
2023-05-17 13:30:35 -0400

    Versão Inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+var FormButton = window.document.getElementById("form-button")
+
+function ButtonHighlightRed() {
+   FormButton.style="background-color: #ff0000; transform: transalate(Y) -15px; transition-duration: 0.5s; box-shadow: 0px 25px 20px #a81b1ba6;" 
+}
+
+function ButtonHighlightOriginal() {
+    FormButton.style="background-color: #e28e28;"
+ }
